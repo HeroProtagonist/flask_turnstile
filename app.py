51b349d409782a1bc27cdbb3dd87d7a4548f0cfa
HeroProtagonist <jtepper@rippling.com>
2024-08-29 16:37:40 -0400

    Add styles and invisible mode

--- app.py
+++ app.py
@@ -10,15 +10,33 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route("/")
 def index():
-    return render_template("index.html", sitekey=os.getenv("TURNSTILE_SITE_KEY"))
+    widget_mode="managed"
+    return render_template("index.html", sitekey=os.getenv("TUNSTILE_MANAGED_SITE_KEY"), widget_mode=widget_mode, nav_classes=_nav_classes(widget_mode))
+
+@app.route("/invisible")
+def invisible():
+    widget_mode="invisible"
+    return render_template("index.html", sitekey=os.getenv("TUNSTILE_INVISIBLE_SITE_KEY"), widget_mode=widget_mode, nav_classes=_nav_classes(widget_mode))
 
 @app.post("/check-challenge")
 def check_challenge():
-    breakpoint()
     data = request.get_json(force=True)
 
-    return _check_challenge(data["token"])
+    return _check_challenge(token=data["token"], widget_mode=data["widget_mode"])
 
-def _check_challenge(token):
-     r = requests.post('https://challenges.cloudflare.com/turnstile/v0/siteverify', data={"secret": os.getenv("TUNSTILE_SECRET_KEY"), "response": token})
+def _check_challenge(token, widget_mode):
+     secret = os.getenv("TUNSTILE_MANAGED_SECRET_KEY")
+
+     if widget_mode == "invisible":
+         secret = os.getenv("TUNSTILE_INVISIBLE_SECRET_KEY")
+
+     r = requests.post('https://challenges.cloudflare.com/turnstile/v0/siteverify', data={"secret": secret, "response": token})
      return r.json()
+
+def _nav_classes(widget_mode):
+    classes = ["nav-link active", "nav-link"]
+
+    if widget_mode == "invisible":
+         classes = reversed(classes)
+
+    return list(classes)
