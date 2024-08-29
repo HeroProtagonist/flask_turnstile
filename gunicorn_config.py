34e82532231e56539ac10e7ab0a7044f09af763d
HeroProtagonist <jtepper@rippling.com>
2024-08-29 13:11:08 -0400

    add gunicorn

---
+++ gunicorn_config.py
@@ -0,0 +1,13 @@
+# https://developers.redhat.com/articles/2023/08/17/how-deploy-flask-application-python-gunicorn#the_application
+
+import os
+
+workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
+
+threads = int(os.environ.get('GUNICORN_THREADS', '4'))
+
+bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
+
+forwarded_allow_ips = '*'
+
+secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
